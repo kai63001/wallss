29479c7c676f2c27159033cb1afcb6ab1c5ad826
kai63001 <animehq5@gmail.com>
2021-02-23 23:51:51 +0700

    change name site

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,23 +1,23 @@
 import { NextSeo } from "next-seo";
 const Header = (props) => (
   <NextSeo
-    title={props.title ? `${props.title} Wallpapers HD Background Images` : "MrWalli HD 4k Beautiful Wallpapers Backgrounds"}
-    description={props.des ? props.des : "MrWalli the best website to download beautiful wallpapers backgrounds images hd 4k desktops phones iphone and tables free"}
-    canonical={`https://mrwalli.com${props.can}`}
+    title={props.title ? `${props.title} Wallpapers HD Background Images` : "Wallss HD 4k Beautiful Wallpapers Backgrounds"}
+    description={props.des ? props.des : "Wallss the best website to download beautiful wallpapers backgrounds images hd 4k desktops phones iphone and tables free"}
+    canonical={`https://wallss.com${props.can}`}
     openGraph={{
         type: 'website',
         locale: 'th_TH',
-        title: props.title ? `${props.title} Wallpapers HD Background Images` : "MrWalli HD 4k Beautiful Wallpapers Backgrounds",
-        description: props.des ? props.des : "MrWalli the best website to download beautiful wallpapers backgrounds images hd 4k desktops phones iphone and tables free",
+        title: props.title ? `${props.title} Wallpapers HD Background Images` : "Wallss HD 4k Beautiful Wallpapers Backgrounds",
+        description: props.des ? props.des : "Wallss the best website to download beautiful wallpapers backgrounds images hd 4k desktops phones iphone and tables free",
         images: [
           {url: props.image == undefined ||props.image.length == 0?'https://cdn.wallpapersafari.com/55/92/SDVHx6.jpg':props.image}
         ],
-        url: `https://mrwalli.com${props.can}`,
+        url: `https://wallss.com${props.can}`,
         site_name: 'MrWalli',
       }}
       twitter={{
-        handle: '@mrwalli',
-        site: '@mrwalli',
+        handle: '@wallss',
+        site: '@wallss',
         cardType: 'summary_large_image',
       }}
   />
