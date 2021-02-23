04b0d189691fd94e383cbc92d545bab34d53a7d8
kai63001 <animehq5@gmail.com>
2021-02-24 02:12:43 +0700

    add backfround

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      asd
+      <div>
+      <Image
+        src="/static/images/wallpaper4.jpg"
+        alt="Walls free wallpaper background image hd"
+        width={100}
+        height={43}
+        layout="responsive"
+      />
+      </div>
+      asdas
     </Layout>
   )
 }
