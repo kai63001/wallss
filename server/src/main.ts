d26c9f793129e0d0645ad630b82557144f8c3d37
kai63001 <animehq5@gmail.com>
2021-02-23 19:00:19 +0700

    add next js

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!fuck off yepsaa')
+    res.send('Helloss World!fuck off yepsaaa')
 })
 
-app.listen(3000)
+app.listen(4000)
