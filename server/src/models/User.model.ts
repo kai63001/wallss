e02d20733845dc3dd6c41bb9269f62879b067f1b
kai63001 <animehq5@gmail.com>
2021-03-01 21:41:23 +0700

    update model

--- server/src/models/User.model.ts
+++ server/src/models/User.model.ts
@@ -1,7 +1,18 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
 })
 
 const User = mongoose.model('User', UserSchema);
--- server/src/models/Wallpaper.model.ts
+++ server/src/models/Wallpaper.model.ts
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose';
 
 const WallpaperSchema = new mongoose.Schema({
+    resolution: {
+        type: String,
+    },
     image: {
         type: String,
         required: true
+    },
+    date: {
+        type: Number,
+        require: true
     },
     users: [{
         type: mongoose.Schema.Types.ObjectId,
