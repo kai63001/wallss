c4fb47f8ae4431d9f1a92c19838217f3b26f6c92
kai63001 <animehq5@gmail.com>
2021-03-02 16:02:58 +0700

    update banner

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 import styles from "../styles/Home.module.sass";
 
 export default function Home() {
-  const image = ['https://images4.alphacoders.com/113/1133943.png','https://images.alphacoders.com/113/1133684.jpg','https://images4.alphacoders.com/113/1133047.jpg']
+  const image = [
+    "https://images4.alphacoders.com/113/1133943.png",
+    "https://images.alphacoders.com/113/1133684.jpg",
+    "https://images4.alphacoders.com/113/1133047.jpg",
+  ];
   return (
     <Layout>
       <br />
@@ -14,20 +18,34 @@ export default function Home() {
       <br />
       <div className="main2">
         <div className="container">
-        {image.map(i=> {
-          return (<div className={styles.mainImage}>
-          <Image
-            src={i}
+          {image.map((i) => {
+            return (
+              <div className={styles.mainImage}>
+                <Image
+                  className="imageRadius"
+                  src={i}
+                  alt="Picture of the author"
+                  width={500}
+                  height={300}
+                  quality={100}
+                  layout="intrinsic"
+                />
+              </div>
+            );
+          })}
+        </div>
+        {/* จบส่วนบน */}
+        <br/>
+        <br/>
+        <Image
+            className="imageRadius"
+            src="/static/images/banner.jpg"
             alt="Picture of the author"
-            width={500}
+            width={1300}
             height={300}
             quality={100}
             layout="intrinsic"
           />
-        </div>)
-        })}
-          
-        </div>
       </div>
     </Layout>
   );
