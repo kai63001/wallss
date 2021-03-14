1663799f882724ec69e03acb1260b82d50ebf541
kai63001 <animehq5@gmail.com>
2021-03-14 18:25:52 +0700

    update upload page

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 const Navbar = (props) => (
   <nav className="navbar">
     <div className={"container main"}>
-      <div className={"logo"}>WALLSS</div>
+      <div className={"logo"}><Link href="/"><a>WALLSS</a></Link></div>
       <div className={"search"}>
         <form action="" method="get">
           <input type="text" className="searchinput" placeholder="Search.."/>
         </form>
       </div>
       <div className={"rightOne"}>
-        <Link href="/">
+        <Link href="/upload">
           <a className="main-btn"> <i className="fas fa-upload"></i> Upload</a>
         </Link>
       </div>
       <div className={"right"}>
-        <Link href="/">
+        <Link href="/register">
           <a className="dark-btn">Create Account</a>
         </Link>
       </div>
---
+++ client/pages/upload.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const upload = () => {
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+export default upload;
