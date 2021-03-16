acc0c1dc96f70add8aced5d795ce1d3c8d983170
kai63001 <animehq5@gmail.com>
2021-03-17 02:52:20 +0700

    up

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,13 +1,14 @@
 import Layout from "../components/Layout";
+import styles from "../styles/Register.module.sass";
 
 const Register = () => {
-    return (
-        <Layout>
-            <div className="main2">
-                ads
-            </div>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <div className={styles.middle}>
+        <div className={styles.center}>asdasdas asdasdas asda</div>
+      </div>
+    </Layout>
+  );
+};
 
 export default Register;
