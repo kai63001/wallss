d0564823acde6bb58965e361c10bacb6fbf3ce7c
kai63001 <animehq5@gmail.com>
2021-03-28 03:18:29 +0700

    update darkmode transition delay .5s

--- client/util/ThemeConfig.ts
+++ client/util/ThemeConfig.ts
@@ -28,32 +28,37 @@ export const GlobalStyles = createGlobalStyle`
   .inputColor {
     background: ${({ theme }) => theme.input};
     color: ${({ theme }) => theme.text};
-    
+    transition: all 0.50s linear;
   }
 
   .inboxColor {
     background: ${({ theme }) => theme.navbar};
+    transition: all 0.50s linear;
   }
   span {
     color: ${({ theme }) => theme.text};
-
+    transition: all 0.50s linear;
   }
 
   .main-btn-outBack2 {
     background-color: ${({ theme }) => theme.body};
+    transition: all 0.50s linear;
     color: ${({ theme }) => theme.text};
   }
 
   .dark-btn-moon {
     color: ${({ theme }) => theme.body};
+    transition: all 0.50s linear;
     background-color: ${({ theme }) => theme.text};
   }
 
   .box {
+    transition: all 0.50s linear;
     background-color: ${({ theme }) => theme.navbar};
   }
 
   .navbar {
+    transition: all 0.50s linear;
     background-color: ${({ theme }) => theme.navbar};
     
   }
@@ -61,7 +66,8 @@ export const GlobalStyles = createGlobalStyle`
  .navbar .logo{
         grid-column: span 1;
         font-size: 30px;
-        margin-top: -5px;
+    transition: all 0.50s linear;
+    margin-top: -5px;
         background: ${({ theme }) => theme.text};
         background: -webkit-linear-gradient(to right, ${({ theme }) =>
           theme.text} 65%, #EF1C83 75%, rgba(251,94,58,1) 100%);
@@ -74,6 +80,7 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   footer {
+    transition: all 0.50s linear;
     background-color: ${({ theme }) => theme.navbar};
   }
 
