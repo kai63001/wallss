8769d81eaeda4436ea33b4f4329a5e7bb9d083bf
kai63001 <animehq5@gmail.com>
2021-03-28 23:50:13 +0700

    update upload page

--- client/pages/upload.tsx
+++ client/pages/upload.tsx
@@ -2,6 +2,7 @@ import Layout from "@/components/Layout";
 import { veriftToken } from "@/middleware/auth.middleware";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import styles from "@/styles/Upload.module.sass";
 
 const upload = () => {
   const router = useRouter();
@@ -15,8 +16,30 @@ const upload = () => {
     return <Layout></Layout>;
   }
 
-  return <Layout>asdasdsa</Layout>;
-
+  return (
+    <Layout>
+      <div className={styles.main}>
+        <div className="center">
+          <h1>Upload Wallpapers</h1>
+        </div>
+        <div className="box">
+          <p>
+            Thanks for contributing to our wallpaper collection! Please review
+            our community rules and remember that all uploads are moderated.
+            Adding tags and a caption to your uploads will help other users find
+            your content easily.
+          </p>
+          <br />
+          <b>Remember:</b>
+          <ul>
+            <li>No selfies or personal photos</li>
+            <li>No screenshots</li>
+            <li>No offensive images</li>
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default upload;
