de7ee3fb7e84c8a90298598237a0cb4745aafa70
kai63001 <animehq5@gmail.com>
2021-03-30 23:03:46 +0700

    fix fs model

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,16 @@
 const path = require("path");
 
 module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
