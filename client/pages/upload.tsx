b9a5a2a3a92270230e9603099b9a9aded041118c
kai63001 <animehq5@gmail.com>
2021-04-13 12:21:26 +0700

    add input value

--- client/pages/upload.tsx
+++ client/pages/upload.tsx
@@ -1,18 +1,30 @@
 import Layout from '@/components/Layout';
 import { veriftToken } from '@/middleware/auth.middleware';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styles from '@/styles/Upload.module.sass';
 
 const upload = () => {
     const router = useRouter();
     const [image, setImage] = useState([]);
+    const [countUpload, setCountUpload] = useState(0)
+    const [dataInput, setDataInput] = useState({});
+    const [errorFile, setErrorFile] = useState(false);
     useEffect(() => {
         console.log('check');
         if (veriftToken() == null) {
             router.push('/auth/login');
         }
     });
+
+    const hanndleOnInputChange = (e) => {
+        const { name, value } = e.target;
+        setDataInput({
+            ...dataInput,
+            [name]: value,
+        });
+    };
 
     const uploadImage = (e) => {
         const files = e.target.files || e.dataTransfer.files;
@@ -22,7 +34,6 @@ const upload = () => {
             console.log('readANdPrv');
             const reader = new FileReader();
             reader.onload = (e) => {
-                console.log(e.target.result);
                 setImage((image) => [
                     ...image,
                     {
@@ -34,11 +45,27 @@ const upload = () => {
             };
             reader.readAsDataURL(file);
         };
+        let count = countUpload;
+        let data = dataInput
         if (files) {
             for (let i = 0; i < files.length; i++) {
                 readAndPreview(files[i]);
+                data = {
+                    ...data,
+                    ['name_'+count]: 'romeoo',
+                    ['tags_'+count]: 'romeoo',
+                }
+                
+                count += 1
+                console.log(dataInput)
             }
+            setDataInput(data)
+            setCountUpload(count);
         }
+    };
+
+    const summitUpload = () => {
+        console.log(image);
     };
 
     if (veriftToken() == null) {
@@ -66,30 +93,58 @@ const upload = () => {
                     </ul>
                     <br />
                     {image.map((e, i) => (
-                        <div className={'container'}>
-                            <div key={i} className={styles.images}>
-                                <img src={e.base64} width='100%' alt='' />
+                        <div key={i} className={'container'}>
+                            <div className={styles.images}>
+                                <img src={e.base64} className='imageRadius wallpaperLoading' width='100%' alt='' />
                             </div>
                             <div className={styles.detail}>
-                                <input type='text' className='main-input inputColor' placeholder='Title, caption or description' />
+                                <input
+                                    type='text'
+                                    onChange={hanndleOnInputChange}
+                                    name={`name_${i}`}
+                                    className='main-input inputColor'
+                                    placeholder='Title, caption or description'
+                                />
+                                <input
+                                    type='text'
+                                    onChange={hanndleOnInputChange}
+                                    name={`tags_${i}`}
+                                    className='main-input inputColor'
+                                    placeholder='Tags ,separated by comma'
+                                />
                             </div>
                         </div>
                     ))}
                     <br />
+                    <p className='text-error'>Error: Support only png, jpg, jpeg, gif</p>
                     <br />
-                    <label htmlFor='file' className='dark-btn m-0'>
-                        Upload Wallpaper
-                    </label>
-                    <input
-                        id='file'
-                        name='file'
-                        type='file'
-                        className={styles.uploadBTN}
-                        onChange={uploadImage}
-                        multiple={true}
-                    />
-                    <br />
-                    <br />
+                    <div className={styles.spaceBTW}>
+                        <div>
+                            <label htmlFor='file' className='dark-btn m-0'>
+                                Upload Wallpaper
+                            </label>
+                            <input
+                                id='file'
+                                name='file'
+                                type='file'
+                                className={styles.uploadBTN}
+                                onChange={uploadImage}
+                                multiple={true}
+                            />
+                        </div>
+                        <div className={styles.inBTW}>
+                            {image.length > 0 ? (
+                                <div onClick={()=>(console.log(dataInput))} className='main-btn m-0'>
+                                    Done
+                                </div>
+                            ) : (
+                                <div>
+                                    <br />
+                                    <br />
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
             <br />
