0e3e944cb8c3038f25ed26cb0a6c0895c766bc1b
kai63001 <animehq5@gmail.com>
2021-05-16 21:44:22 +0700

    update

--- server/src/query/category.query.ts
+++ server/src/query/category.query.ts
@@ -30,17 +30,7 @@ export const categoryPage = async (
         lean: true
       };
 
-      const data = await Wallpaper.paginate({}, options, async function (err, result) {
-        // result.itemsList [here docs become itemsList]
-        // result.paginator.itemCount = 100 [here totalDocs becomes itemCount]
-        // result.paginator.perPage = 10 [here limit becomes perPage]
-        // result.paginator.currentPage = 1 [here page becomes currentPage]
-        // result.paginator.pageCount = 10 [here totalPages becomes pageCount]
-        // result.paginator.next = 2 [here nextPage becomes next]
-        // result.paginator.prev = null [here prevPage becomes prev]
-        // result.paginator.slNo = 1 [here pagingCounter becomes slNo]
-        // result.paginator.hasNextPage = true
-        // result.paginator.hasPrevPage = false
+      const data = await Wallpaper.paginate({categoly:{$regex: "^"+category+"$", $options: 'i'}}, options, async function (err, result) {
         console.log(result)
         return result
       });
